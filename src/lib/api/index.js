8cf2304f4767776a1aadd8d786e5b5a6d0edad83
Mar <marlene.gmolina@gmail.com>
2020-07-08 19:14:06 -0500

    [added] endpoint to get promotion by product

--- src/lib/api/index.js
+++ src/lib/api/index.js
@@ -129,11 +129,31 @@ async function postScan (qr, promotionId, token) {
   }
 }
 
+// verifica y saca las promociones del qr
+async function getPromotionsByQr (token, qr) {
+  try {
+    const response = await fetch(`${urlBase}/scans/${qr}/promotions`, {
+      headers: {
+        Authorization: token
+      }
+    })
+    const parsedJson = await response.json()
+    if (!parsedJson.success) {
+      throw Error(parsedJson.error)
+    }
+    return parsedJson.data.promotions
+  } catch (error) {
+    console.error(error.message)
+    throw error
+  }
+}
+
 export default {
   getAllPromotions,
   getPromotionById,
   getPromotionsScansByUser,
   postScan,
   signup,
-  login
+  login,
+  getPromotionsByQr
 }
